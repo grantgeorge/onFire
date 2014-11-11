8decc1b4f2749d4d1b9affccfcd09cf425519b12
Grant George <grantelliottgeorge@gmail.com>
2014-11-10 23:45:32 -0500

    manual images :(

--- Gruntfile.js
+++ Gruntfile.js
@@ -239,7 +239,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          src: '{,*/}*.{png,jpg,jpeg}',
           dest: '<%= yeoman.dist %>/images'
         }]
       }
